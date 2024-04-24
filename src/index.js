77436aeec40844acad670736e90c2e35c8a338cc
cristian <cchura.cpc@gmail.com>
2024-04-23 22:08:19 -0400

    base de datos

--- src/index.js
+++ src/index.js
@@ -1,14 +1,67 @@
+require('dotenv').config()
 const express = require("express");
+const { Sequelize, DataTypes } = require('sequelize');
 
 const app = express();
 
-app.get("/api/usuarios", (req, res) => {
-    return res.json([
-        {id: 1, name: "Admin", email: "admin@mail.com"},
-        {id: 2, name: "Juan", email: "juan@mail.com"}
-    ]);
+app.get("/", (req, res) => {
+    return res.json({mensaje: "Hola Mundo"});
+});
+
+app.get("/api/usuarios", async (req, res) => {
+
+    const usuarios = await User.findAll();
+
+    return res.json(usuarios);
 })
 
-app.listen(3000, () => {
-    console.log("Servidor iniciado de NODE");
+const PUERTO = process.env.PORT || 3000
+
+// conectando CON BD
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(process.env.BD_DATABASE, process.env.BD_USER, process.env.BD_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+async function testConexionBD(){
+    try {
+        await sequelize.authenticate();
+        console.log('CONEXION CORRECTA DE BD.');
+      } catch (error) {
+        console.error('ERROR DE CONEXION DE BD:', error);
+      }
+}
+
+testConexionBD()
+
+const User = sequelize.define(
+    'User',
+    {
+      // Model attributes are defined here
+      nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        // allowNull defaults to true
+      },
+      password: {
+        type: DataTypes.STRING,
+        // allowNull defaults to true
+      },
+    },
+    {
+      // Other model options go here
+    },
+  );
+  
+  // `sequelize.define` also returns the model
+  // console.log(User === sequelize.models.User); 
+  User.sync()
+
+
+app.listen(PUERTO, () => {
+    console.log("Servidor iniciado de NODE http://localhost:"+PUERTO);
 })
